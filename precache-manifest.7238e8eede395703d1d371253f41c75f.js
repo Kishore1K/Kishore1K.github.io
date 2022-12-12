9245f1bcf0e8f2868441fc122c29a5613c0983b4
Kishore1K <reddykishore527@gmail.com>
2022-12-12 21:42:40 +0530

    Updates

---
+++ precache-manifest.7238e8eede395703d1d371253f41c75f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b927ca7d0bf728d7f232d08267d866f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6cd897a8887c7b111c3",
+    "url": "/static/css/main.a02b7dbb.chunk.css"
+  },
+  {
+    "revision": "438bd9c8c7225d24bc55",
+    "url": "/static/js/2.f58cbe24.chunk.js"
+  },
+  {
+    "revision": "b6cd897a8887c7b111c3",
+    "url": "/static/js/main.ecee11ec.chunk.js"
+  },
+  {
+    "revision": "0437ccf3fcfeaa1a5de9",
+    "url": "/static/js/runtime-main.c1483060.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "da598869ba5d8cacd0d5c2fba8757277",
+    "url": "/static/media/Exam.da598869.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "93c656a129c17b1179851b24d5728c60",
+    "url": "/static/media/examAndroid.93c656a1.png"
+  },
+  {
+    "revision": "9ce8783cd5f0d42e3bd4ef62461291e8",
+    "url": "/static/media/locallibrary.9ce8783c.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.52be3144.svg"
+  }
+]);
